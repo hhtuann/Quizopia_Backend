d613487143d9c8e67f1e953483e85a29696feb3b
Hoàng Hoàng Tuấn <hhtuann.19@gmail.com>
2026-06-26 18:20:59 +0700

    Init Spring Boot backend

---
+++ src/main/java/com/hhtuann/backend/QuizopiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hhtuann.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizopiaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizopiaBackendApplication.class, args);
+    }
+
+}
